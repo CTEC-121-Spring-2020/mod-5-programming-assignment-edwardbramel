210f6c284d99085fd52c9a364693a88e58e37bed
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-25 15:13:35 -0700

    i want problem 1 to have input

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,25 +2,53 @@
 #   Programming Assignment 5
 #       Prob-1.py
 
-# <YOUR NAME>
+# Eddy
 
-# IPO
+# a number between 1 and 10 returns a string representing the Roman numeral
 
 # function definition
 
 # unit test function
-def unitTest():
-    print("\nUnit Tests")
-    
-    # code here
 
+
+def romanN(number):
+    if number == 1:
+        return 'I'
+    elif number == 2:
+        return 'II'
+    elif number == 3:
+        return 'III'
+    elif number == 4:
+        return 'IV'
+    elif number == 5:
+        return "V"
+    elif number == 6:
+        return "VI"
+    elif number == 7:
+        return "VII"
+    elif number == 8:
+        return "VIII"
+    elif number == 9:
+        return "IX"
+    elif number == 10:
+        return "X"
+    else:
+        return "unknown"
+
+
+def unitTest():
+    print("your Roman numeral is: ", romanN(1))
+    print("your Roman numeral is: ", romanN(2))
+    print("your Roman numeral is: ", romanN(3))
+    print("your Roman numeral is: ", romanN(4))
+    print("your Roman numeral is: ", romanN(5))
+    print("your Roman numeral is: ", romanN(6))
+    print("your Roman numeral is: ", romanN(7))
+    print("your Roman numeral is: ", romanN(8))
+    print("your Roman numeral is: ", romanN(9))
+    print("your Roman numeral is: ", romanN(10))
+    print("your Roman numeral is: ", romanN(0))
     print()
 
-def main():
-    # replace the code below with your code
-    print("Main")    
-
-    # your code here
 
 unitTest()
-main()
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,7 +2,7 @@
 #   Programming Assignment 5
 #       Prob-2.py
 
-# <YOUR NAME>
+# Eddy
 
 # IPO
 
